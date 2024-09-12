af4eeb6d313a9f4c9c107ec31c1a3e03e7683262
Manuel Molina <manuelmolinapena@gmail.com>
2024-09-12 17:07:48 -0600

    [Feat]: Basics.

--- lib/event_bridge-stack.ts
+++ lib/event_bridge-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EventBridgeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const eventLambda = new cdk.aws_lambda.Function(this, 'EventLambdaHandlerFunction', {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+      code: cdk.aws_lambda.Code.fromInline(`
+          exports.handler = async (event) =>{
+            console.log('Event received: ', event)
+            return {}
+          }        
+        `),
+      handler: 'index.handler'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EventBridgeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    })
+
+    const customEventBus = new cdk.aws_events.EventBus(this, 'CustomEventBus', {
+      eventBusName: 'MyCustomBus'
+    })
+
+    const rule = new cdk.aws_events.Rule(this, 'EventLambdaHandlerRule', {
+      eventPattern: {
+        source: ['custom.ecommerce'],
+        detailType: ['OrderPlaced'],
+      },
+      eventBus: customEventBus
+    })
+
+    rule.addTarget(new cdk.aws_events_targets.LambdaFunction(eventLambda))
+
+    new cdk.CfnOutput(this, 'EventBridgeBusArn', {
+      value: customEventBus.eventBusArn
+    })
   }
 }
